06f5b253ae472f7ddc1e7e1a28a925d81fced055
castle-bird <seongjo0501@gmail.com>
2026-08-10 21:29:03 +0900

    refactor: PostTagRepository 키 타입 변경
    - PostTagRepository의 기본 키 타입을 Long에서 PostTagId로 수정 (복합키)

--- src/main/java/com/castlebird/blog/post/repository/PostTagRepository.java
+++ src/main/java/com/castlebird/blog/post/repository/PostTagRepository.java
@@ -1,9 +1,10 @@
 package com.castlebird.blog.post.repository;
+import com.castlebird.blog.post.entity.PostTagId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.castlebird.blog.post.entity.PostTag;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostTagRepository extends JpaRepository<PostTag, Long> {
+public interface PostTagRepository extends JpaRepository<PostTag, PostTagId> {
 
 }
